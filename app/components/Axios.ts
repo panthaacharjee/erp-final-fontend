38894474147c5610b401b7223a696c7113223743
Pantha Acharjee <freelancerpantha@gmail.com>
2025-07-29 12:47:11 +0600

    first commit

--- app/components/Axios.ts
+++ app/components/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-    baseURL:"https://erp-final-1.onrender.com/api/v1"
+    baseURL:"http://localhost:4000/api/v1"
 })
 
 export default Axios
