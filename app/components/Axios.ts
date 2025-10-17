b23d0fdd0c695965b848a285161cf9e515fc5787
Pantha Acharjee <freelancerpantha@gmail.com>
2025-10-17 12:02:54 +0600

    first commit

--- app/components/Axios.ts
+++ app/components/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const staticUrl = `https://erp-final-1.onrender.com`;
-const staticUrl = `http://localhost:4000`;
+const staticUrl = `https://erp-final-1.onrender.com`;
+// const staticUrl = `http://localhost:4000`;
 const Axios = axios.create({
   baseURL: `${staticUrl}/api/v1`,
 });
